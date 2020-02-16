0d4cf3b351469dd9cfbdb9d76c9e116d878d16a5
Sage Gerard <sage@sagegerard.com>
2020-02-16 11:12:57 -0500

    Remove test pkg dependency for metapkg

--- unlike-assets/info.rkt
+++ unlike-assets/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "unlike-assets-lib" "unlike-assets-doc" "unlike-assets-test"))
+(define deps '("base" "unlike-assets-lib" "unlike-assets-doc"))
 (define implies '("unlike-assets-lib" "unlike-assets-doc"))
 (define pkg-desc "A meta-package for unlike-assets-*")
 (define pkg-authors '(sage))

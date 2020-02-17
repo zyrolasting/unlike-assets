9bcb66b83d61ccfc84098391c93fbcc43b3c4b9d
Sage Gerard <sage@sagegerard.com>
2020-02-17 11:10:11 -0500

    Add missing doc dependencies

--- unlike-assets-doc/info.rkt
+++ unlike-assets-doc/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "unlike-assets-lib"))
+(define deps '("base" "unlike-assets-lib" "kinda-ferpy" "graph-lib"))
 (define build-deps '("unlike-assets-lib" "scribble-lib" "racket-doc"))
 (define update-implies '("unlike-assets-lib"))
 (define pkg-desc "Documentation part of unlike-assets")

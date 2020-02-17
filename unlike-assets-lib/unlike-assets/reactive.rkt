b6e3a99911dce5994a2856194f86e038e5e90ff8
Sage Gerard <sage@sagegerard.com>
2020-02-17 16:24:19 -0500

    Extract reactive test submodule

--- unlike-assets-lib/unlike-assets/reactive.rkt
+++ unlike-assets-lib/unlike-assets/reactive.rkt
@@ -82,31 +82,3 @@
   (if (stop? v)
       v
       (apply-until (v) stop?)))
-
-(module+ test
-  (require rackunit)
-
-  (test-pred "Can recognize live build procedures"
-             live-build?
-             (start-live-build "" void void))
-
-  (test-pred "Can recognize build systems"
-             u/a-build-system?
-             (make-u/a-build-system void))
-
-  (test-case "Live builds start immediately"
-    (define ch (make-channel))
-    (define build (start-live-build "" void (λ _ (channel-put ch 'what))))
-    (define deadline (alarm-evt (+ (current-inexact-milliseconds) 500)))
-    (check-eq? 'what (sync deadline ch)))
-
-  (test-case "Build systems store encountered live builds"
-    (define sys (make-u/a-build-system
-                 (λ (key _) (start-live-build ""
-                                              (λ _ key)
-                                              (λ _ key)
-                                              equal?))))
-    (check-equal? (make-hash) (sys))
-    (sys "a")
-    (check-true (hash-has-key? (sys) "a")
-                (live-build? (hash-ref (sys) "a")))))
---
+++ unlike-assets-test/tests/unlike-assets/reactive.rkt
@@ -0,0 +1,29 @@
+#lang racket/base
+
+(require rackunit
+         unlike-assets/reactive)
+
+(test-pred "Can recognize live build procedures"
+           live-build?
+           (start-live-build "" void void))
+
+(test-pred "Can recognize build systems"
+           u/a-build-system?
+           (make-u/a-build-system void))
+
+(test-case "Live builds start immediately"
+  (define ch (make-channel))
+  (define build (start-live-build "" void (λ _ (channel-put ch 'what))))
+  (define deadline (alarm-evt (+ (current-inexact-milliseconds) 500)))
+  (check-eq? 'what (sync deadline ch)))
+
+(test-case "Build systems store encountered live builds"
+  (define sys (make-u/a-build-system
+               (λ (key _) (start-live-build ""
+                                            (λ _ key)
+                                            (λ _ key)
+                                            equal?))))
+  (check-equal? (make-hash) (sys))
+  (sys "a")
+  (check-true (hash-has-key? (sys) "a")
+              (live-build? (hash-ref (sys) "a"))))

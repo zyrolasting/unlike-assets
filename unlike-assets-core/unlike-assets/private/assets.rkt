926938c949eedb947d95a7faef9f8d30868427df
Sage Gerard <sage@sagegerard.com>
2020-04-12 16:54:23 -0400

    Add contracts and preamble to assets.rkt

--- unlike-assets-core/unlike-assets/private/assets.rkt
+++ unlike-assets-core/unlike-assets/private/assets.rkt
@@ -1,12 +1,37 @@
 #lang racket/base
 
+#|
+Can you keep a secret? An asset is just a hash.
+
+When you do something weird, like try to import a non-Racket thing
+into Racket, as if it's a module, you have no idea what you're going
+to get. Unless you want to involve fancy static analysis tricks,
+you can't derive struct definitions or guarentee type safety.
+Hashes are just an off-the-shelf way to capture the data fields
+of an asset.
+
+Everything in this module is about giving hashes structure with
+contracts and semantic means for grabbing fields, so that you
+can enforce reasonable constraints and maintain readable code.
+|#
+
 (require racket/contract)
 (provide asset/c
-         asset?
          asset
-         make-asset
-         make-asset-contract
-         make-u/a-procure-procedure)
+         (contract-out [asset? (-> any/c boolean?)]
+                       [make-asset (-> (and/c immutable? hash?)
+                                       (case->
+                                        (-> (and/c immutable? hash?))
+                                        (-> symbol? any/c)
+                                        (-> symbol? (-> any) any)))]
+                       [make-asset-contract
+                        (->* (#:allow-missing-keys? any/c
+                              (non-empty-listof
+                               (cons/c symbol?
+                                       (-> asset? any/c))))
+                             boolean?)]
+                       [make-u/a-procure-procedure
+                        (-> u/a-build-system? (->* (string?) #:rest (listof symbol?) any))]))
 
 (require racket/string
          kinda-ferpy
@@ -16,10 +41,11 @@
 
 (define (make-asset-contract #:allow-missing-keys? weak? pairings)
   (λ (maybe-matching)
-    (andmap (λ (pair)
-              (with-handlers ([exn:fail? (λ _ weak?)])
-                ((cdr pair) (maybe-matching (car pair)))))
-            pairings)))
+    (and (andmap (λ (pair)
+                   (with-handlers ([exn:fail? (λ _ weak?)])
+                     ((cdr pair) (maybe-matching (car pair)))))
+                 pairings)
+     #t)))
 
 (define-syntax (asset/c stx)
   (syntax-parse stx
@@ -34,7 +60,7 @@
 
 (define (make-asset h)
   (procedure-rename
-   (case-lambda [()    (hash-keys h)]
+   (case-lambda [()    h]
                 [(k)   (hash-ref h k)]
                 [(k t) (hash-ref h k t)])
    'asset-ref))

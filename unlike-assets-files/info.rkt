24a545682cc0fcfe09d8855a4708d327a33e180a
Sage Gerard <sage@sagegerard.com>
2020-04-13 13:04:43 -0400

    Reconfigure info.rkt in unlike-assets-files

--- unlike-assets-files/info.rkt
+++ unlike-assets-files/info.rkt
@@ -1,6 +1,5 @@
 #lang info
 (define collection 'multi)
-(define deps '("base" "unlike-assets-lib"))
+(define deps '("base" "unlike-assets-resolver"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/xyoyx.scrbl" ())))
-(define pkg-desc "Filesystem integration for polyglot2")
+(define pkg-desc "Filesystem integration for unlike-assets")

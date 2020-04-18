f1fe1fa76338c32cabb24de6e5ffe06af3300c8a
Sage Gerard <sage@sagegerard.com>
2020-04-17 22:39:57 -0400

    Reprovide all project bindings in u/a lang

--- unlike-assets/u/a.rkt
+++ unlike-assets/u/a.rkt
@@ -3,10 +3,10 @@
 (require (for-syntax racket/base)
          racket/runtime-path
          unlike-assets/resolver
-         unlike-assets/projects/cli)
+         unlike-assets/projects)
 
 (provide (for-syntax (all-from-out racket/base))
          (all-from-out racket/base
                        racket/runtime-path
                        unlike-assets/resolver
-                       unlike-assets/projects/cli))
+                       unlike-assets/projects))

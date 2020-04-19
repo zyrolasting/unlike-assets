0ebafd05cc5c22e90f33973ffc8b9ff6f45c2a92
Sage Gerard <sage@sagegerard.com>
2020-04-18 23:19:24 -0400

    Allow user overriding of asset production for racket modules

--- unlike-assets-racket-modules/unlike-assets/racket-modules.rkt
+++ unlike-assets-racket-modules/unlike-assets/racket-modules.rkt
@@ -1,8 +1,10 @@
 #lang racket/base
 
 (require racket/contract
+         racket/function
          racket/rerequire
          racket/path
+         racket/sequence
          unlike-assets/resolver)
 
 (define module-path/c
@@ -11,10 +13,11 @@
 (provide
  module-path/c
  (contract-out
+  [make-asset-from-provides (-> module-path/c asset?)]
   [racket-modules (->* ((or/c path-for-some-system?
                               path-string?
-                              (-> string? module-path/c)))
-                       #:rest (listof any/c)
+                              (-> string? (or/c module-path/c #f))))
+                       ((-> module-path/c asset?))
                        procedure?)]))
 
 (define (normalize-make-module-path v)
@@ -32,13 +35,20 @@
                                 v))
   v))
 
+(define (make-asset-from-provides module-path)
+  (dynamic-require module-path (void))
+  (define-values (exported-variables _) (module->exports module-path))
+  (define phase0 (findf (λ (x) (eq? (car x) 0)) exported-variables))
+  (make-asset
+   (for/hash ([id (sequence-map car (in-list (cdr phase0)))])
+     (values id (dynamic-require module-path id)))))
 
-(define (racket-modules make-module-path . user-data)
+(define (racket-modules make-module-path [make-asset make-asset-from-provides])
   (define normalized (normalize-make-module-path make-module-path))
   (λ (key recurse)
     (let ([module-path (normalized key)])
       (and module-path
            (start-live-build! key
                               #:sample! (λ () (dynamic-rerequire module-path #:verbosity 'none))
-                              #:build! (λ _ (apply (dynamic-require module-path 'make-asset) user-data))
+                              #:build! (λ _ (make-asset module-path))
                               #:suppress? (λ (a b) (null? b)))))))

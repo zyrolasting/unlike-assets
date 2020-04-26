2849b901b752c5d00ee00f2b082774277e051afc
Sage Gerard <sage@sagegerard.com>
2020-04-25 20:05:01 -0400

    Fix collection paths with #lang u/a

--- unlike-assets/u/a.rkt
+++ unlike-assets/u/a.rkt
@@ -2,11 +2,9 @@
 
 (require (for-syntax racket/base)
          racket/runtime-path
-         unlike-assets/resolver
-         unlike-assets/projects)
+         unlike-assets)
 
 (provide (for-syntax (all-from-out racket/base))
          (all-from-out racket/base
                        racket/runtime-path
-                       unlike-assets/resolver
-                       unlike-assets/projects))
+                       unlike-assets))

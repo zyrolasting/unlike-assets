317180fa9bf24bcc6f02a532ac2ffefd97bd273d
Sage Gerard <sage@sagegerard.com>
2020-06-12 21:48:11 -0400

    Remove dependencies

--- unlike-assets/info.rkt
+++ unlike-assets/info.rkt
@@ -2,9 +2,4 @@
 
 (define collection 'multi)
 (define version "2.0")
-(define deps '("base"
-               "idiocket"
-               "reprovide-lang-lib"
-               "hash-partition"
-               "search-upward"
-               "web-server-lib"))
+(define deps '("base" "search-upward"))
